5bce2a75699d78046c56f0c3983e864db234127e
siminostroza <sim.inostroza@duocuc.cl>
2026-05-17 16:20:30 -0400

    Conectar ms_auth con user utilizando openfeing, credenciales

--- services/ms-auth/src/main/java/com/logistica/ms_auth/model/UserCredencial.java
+++ services/ms-auth/src/main/java/com/logistica/ms_auth/model/UserCredencial.java
@@ -2,12 +2,8 @@ package com.logistica.ms_auth.model;
 
 import java.sql.Timestamp;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
